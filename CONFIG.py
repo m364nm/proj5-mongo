72730403518aa8f94414c4212ac1a55c63c2126d
Megan McMillan <mcmillan@ix-trusty.cs.uoregon.edu>
2016-02-12 00:06:36 -0800

    modified for ix

--- CONFIG.py
+++ CONFIG.py
@@ -5,15 +5,16 @@ Edit to fit development or deployment environment.
 """
 
 ### Flask settings
-PORT=5000        # The port I run Flask on
-DEBUG = True     # Set to False on ix
+PORT=6291        # The port I run Flask on
+SERVER_NAME = "ix.cs.uoregon.edu:{}".format(PORT)
+DEBUG = False     # Set to False on ix
 
 ### MongoDB settings
-MONGO_PORT=27017 #  Probably best to use the same port as you use on ix
+MONGO_PORT=4570 #  Probably best to use the same port as you use on ix
 
 ### The following are for a Mongo user you create for accessing your
 ### memos database.  It should not be the same as your database administrator
 ### account.
 MONGO_PW = "iremember"
 MONGO_USER = "memoproj"
-MONGO_URL = "mongodb://{}:{}@localhost:{}/memos".format(MONGO_USER,MONGO_PW,MONGO_PORT)
+MONGO_URL = "mongodb://{}:{}@ix.cs.uoregon.edu:{}/memos".format(MONGO_USER,MONGO_PW,MONGO_PORT)
